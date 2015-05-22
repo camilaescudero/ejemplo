2f4da015d1734e95a9a37ab9ad292bbfec4444a6
camilaescudero <c.escudero@live.cl>
2015-05-22 15:11:18 -0300

    Initial commit

---
+++ app/src/main/java/camila/myapplication/Secundario.java
@@ -0,0 +1,22 @@
+package camila.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by Cami on 26-04-2015.
+ */
+public class Secundario extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.segundolayout);
+
+    }
+
+}
